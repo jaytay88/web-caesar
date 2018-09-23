a894a090488c666107514cda57154df716bf7a3b
J Taylor <jendt88@gmai.com>
2018-09-23 10:00:17 -0500

    Finish wb caesar

--- main.py
+++ main.py
@@ -9,30 +9,30 @@ form = """
   <html>
     <head>
       <style>
-        form {
+        form {{
             background-color: #eee;
             padding: 20px;
             margin: 0 auto;
             width: 540px;
             font: 16px sans-serif;
             border-radius: 10px;
-        }
+        }}
 
-        textarea {
+        textarea {{
             margin: 10px -;
             width: 540px;
             height: 120px
-        }
+        }}
 
       </style>
     </head>
     <body>
-      <form method=['POST']>
+      <form action="/encrypt" method='POST'>
         <label><strong>Rotate by: </strong>
         <input type="text" name="rot" value="0"></input></label>
         <br>
         <br>
-        <textarea name-"text"></textarea>
+        <textarea name="text">{0}</textarea>
         <br>
         <br>
         <button type="submit" name="Submit query">Submit query</button>
@@ -41,16 +41,18 @@ form = """
   </html>
 """
 
+
+@app.route("/encrypt", methods=['POST'])
+def encrypt():
+  rotation = request.form['rot']
+  rotation = int(rotation)
+  msg = request.form['text']
+  encrypted = rotate_string(msg, rotation)
+  return form.format(encrypted)
+
 @app.route("/")
 def index():
-    return form
-
-@app.route("/", methods=['POST'])
-def encrypt():
-  rotation = request.rot
-  sentence = request.text
-  response = rotate_string(rotation, sentence)
-  return '<h1>' + response + '</h1>'
+    return form.format("")
 
 app.run()
 
